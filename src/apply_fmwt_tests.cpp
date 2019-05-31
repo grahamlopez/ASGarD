92422230130ce3a01c5be4fe5a4c948787fc99eb
Timothy Younkin <tyounkin@Timothys-MacBook-Pro.local>
2019-05-31 09:30:40 -0400

    working version of apply fmwt

--- src/apply_fmwt_tests.cpp
+++ src/apply_fmwt_tests.cpp
@@ -1,7 +1,7 @@
+#include "apply_fmwt.hpp"
 #include "matlab_utilities.hpp"
 #include "pde.hpp"
 #include "tests_general.hpp"
-#include "apply_fmwt.hpp"
 #include "transformations.hpp"
 #include <numeric>
 
@@ -10,201 +10,217 @@ TEMPLATE_TEST_CASE("apply_fmwt", "[apply_fmwt]", double, float)
   auto const relaxed_comparison = [](auto const first, auto const second) {
     auto first_it = first.begin();
     std::for_each(second.begin(), second.end(), [&first_it](auto &second_elem) {
-      auto const f1 = *first_it++; 
-      //std::cout.precision(17);
-      //std::cout << "first and second element " << std::fixed << f1
-      //             << " " << std::fixed << second_elem << std::endl;
-      //std::cout << "diff " << std::fixed << std::abs(f1 - second_elem) << std::endl;
-      //std::cout << "epsilon  "<< std::numeric_limits<TestType>::epsilon() << std::endl;
-      //std::cout << "max  "<< std::max(std::abs(f1),std::abs(second_elem)) << std::endl;
-      //std::cout << "compare " << std::numeric_limits<TestType>::epsilon()*1.0e4*std::max(std::abs(f1),std::abs(second_elem)) << std::endl;
-      auto const diff = std::abs(f1 - second_elem);///std::max(std::abs(f1),std::abs(second_elem));
+      auto const f1       = *first_it++;
+      auto const diff     = std::abs(f1 - second_elem);
       auto const abs_diff = std::abs(f1 - second_elem);
-      REQUIRE(diff < std::numeric_limits<TestType>::epsilon()*1.0e5*std::max(std::abs(f1),std::abs(second_elem)));
-      //REQUIRE(Approx(f1)
-      //            .margin(std::numeric_limits<TestType>::epsilon()) ==
-      //        second_elem);
-      //REQUIRE(std::abs((*first_it++) - second_elem) < 1.0e-5*std::max(std::abs((*first_it++)),std::abs((second_elem))));
+      REQUIRE(diff < std::numeric_limits<TestType>::epsilon() * 1.0e5 *
+                         std::max(std::abs(f1), std::abs(second_elem)));
     });
   };
-  
-  SECTION("Apply fmwt test 1")
-  {
-    std::cout << " type epsilon " << std::numeric_limits<TestType>::epsilon() << std::endl;
-    int kdeg = 2;
-    int lev = 2;
-    std::string out_base =
-        "../testing/generated-inputs/apply_fmwt/";
 
-    std::string mat1_string    = out_base + "mat1_k1_lev2.dat";
-    //std::string fmwt_string    = out_base + "fmwt_k1_lev2.dat";
-    
-    dimension const dim =
-        make_PDE<TestType>(PDE_opts::continuity_1, lev, kdeg)
-            ->get_dimensions()[0];
+  // Testing of various apply fmwt methods
+  // for two size arrays generated (random [0,1]) matrix
+  // is read in and used as the coefficient matrix
+  // the optimized methods (method 2 and 3) are then compared
+  // to the full matrix multiplication (method 1)
+  SECTION("Apply fmwt test set 1 - kdeg=2 lev=2")
+  {
+    int kdeg             = 2;
+    int lev              = 2;
+    std::string out_base = "../testing/generated-inputs/apply_fmwt/";
+
+    std::string mat1_string = out_base + "mat1_k1_lev2.dat";
+
+    dimension const dim = make_PDE<TestType>(PDE_opts::continuity_1, lev, kdeg)
+                              ->get_dimensions()[0];
 
     fk::matrix<TestType> const fmwt = operator_two_scale<TestType>(dim);
 
     fk::matrix<TestType> mat1 =
         fk::matrix<TestType>(read_matrix_from_txt_file(mat1_string));
-    //fk::matrix<TestType> fmwt =
-    //    fk::matrix<TestType>(read_matrix_from_txt_file(fmwt_string));
-    
-    //mat1.print("mat1");	
-    //fmwt.print("fmwt");	
-    int isLeft = 1;
-    int isTrans = 0;
-    int method = 2;
-    fk::matrix<TestType> productLeftFull = fmwt*mat1;
-    auto const productLeft = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productLeftFull.print("full");	
-    productLeft.print("apply left");	
-    
-    SECTION("degree = 4, lev 5 fmwt apply left") { relaxed_comparison(productLeftFull, productLeft); }
-    
-  
-  
-    isLeft = 1;
-    isTrans = 1;
-    method=2;
-    fk::matrix<TestType> fmwt_transpose = fk::matrix<TestType>(fmwt).transpose();
-    fk::matrix<TestType> productLeftTransFull = fmwt_transpose*mat1;
-    auto const productLeftTrans = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    SECTION("degree = 2, lev 2 fmwt apply left transpose method 2") { relaxed_comparison(productLeftTrans, productLeftTransFull); }
-    
-    isLeft = 0;
-    isTrans = 0;
-    fk::matrix<TestType> productRightFull = mat1*fmwt;
-    auto const productRight = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    
-    SECTION("degree = 2, lev 2 fmwt apply right method 2") { relaxed_comparison(productRightFull, productRight); }
-    
-    isLeft = 0;
-    isTrans = 1;
-    fk::matrix<TestType> productRightTransFull = mat1*fmwt_transpose;
-    auto const productRightTrans = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    SECTION("degree = 2, lev 2 fmwt apply right transpose method 2") { relaxed_comparison(productRightTrans, productRightTransFull); }
-    
-    
-    method=3;
-    isLeft = 1;
-    isTrans = 0;
-    auto const productLeft3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productLeftFull.print("full");	
-    productLeft3.print("apply left 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply left method 3") { relaxed_comparison(productLeftFull, productLeft3); }
-    
-    isLeft = 1;
-    isTrans = 1;
-    auto const productLeftTrans3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productLeftTransFull.print("full");	
-    productLeftTrans3.print("apply left 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply left method 3") { relaxed_comparison(productLeftTransFull, productLeftTrans3); }
-    
-    isLeft = 0;
-    isTrans = 0;
-    auto const productRight3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productRightFull.print("full");	
-    productRight3.print("apply left 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply right method 3") { relaxed_comparison(productRightFull, productRight3); }
-    
-    isLeft = 0;
-    isTrans = 1;
-    auto const productRightTrans3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productRightTransFull.print("full");	
-    productRightTrans3.print("apply right trans 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply right trans method 3") { relaxed_comparison(productRightTransFull, productRightTrans3); }
-  }
-  
-  SECTION("Apply fmwt test 2")
-  {
-    int kdeg = 4;
-    int lev = 5;
-    std::string out_base =
-        "../testing/generated-inputs/apply_fmwt/";
 
-    std::string mat1_string    = out_base + "mat1.dat";
-    
-    dimension const dim =
-        make_PDE<TestType>(PDE_opts::continuity_1, lev, kdeg)
-            ->get_dimensions()[0];
+    int isLeft  = 1;
+    int isTrans = 0;
+    int method  = 2;
+    auto const productLeft1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productLeft2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 2, lev 2 fmwt apply left - method 2")
+    {
+      relaxed_comparison(productLeft1, productLeft2);
+    }
+
+    isLeft  = 1;
+    isTrans = 1;
+    method  = 2;
+    auto const productLeftTrans1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productLeftTrans2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 2, lev 2 fmwt apply left transpose - method 2")
+    {
+      relaxed_comparison(productLeftTrans1, productLeftTrans2);
+    }
+
+    isLeft  = 0;
+    isTrans = 0;
+    auto const productRight1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productRight2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+
+    SECTION("degree = 2, lev 2 fmwt apply right - method 2")
+    {
+      relaxed_comparison(productRight1, productRight2);
+    }
+
+    isLeft  = 0;
+    isTrans = 1;
+    auto const productRightTrans1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productRightTrans2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 2, lev 2 fmwt apply right transpose - method 2")
+    {
+      relaxed_comparison(productRightTrans1, productRightTrans2);
+    }
+
+    method  = 3;
+    isLeft  = 1;
+    isTrans = 0;
+    auto const productLeft3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 2, lev 2 fmwt apply left method 3")
+    {
+      relaxed_comparison(productLeft1, productLeft3);
+    }
+
+    isLeft  = 1;
+    isTrans = 1;
+    auto const productLeftTrans3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 2, lev 2 fmwt apply left method 3")
+    {
+      relaxed_comparison(productLeftTrans1, productLeftTrans3);
+    }
+
+    isLeft  = 0;
+    isTrans = 0;
+    auto const productRight3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 2, lev 2 fmwt apply right method 3")
+    {
+      relaxed_comparison(productRight1, productRight3);
+    }
+
+    isLeft  = 0;
+    isTrans = 1;
+    auto const productRightTrans3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 2, lev 2 fmwt apply right trans method 3")
+    {
+      relaxed_comparison(productRightTrans1, productRightTrans3);
+    }
+  }
+
+  SECTION("Apply fmwt test set 2 - kdeg=4 lev=5")
+  {
+    int kdeg             = 4;
+    int lev              = 5;
+    std::string out_base = "../testing/generated-inputs/apply_fmwt/";
+
+    std::string mat1_string = out_base + "mat1.dat";
+
+    dimension const dim = make_PDE<TestType>(PDE_opts::continuity_1, lev, kdeg)
+                              ->get_dimensions()[0];
 
     fk::matrix<TestType> const fmwt = operator_two_scale<TestType>(dim);
 
     fk::matrix<TestType> mat1 =
         fk::matrix<TestType>(read_matrix_from_txt_file(mat1_string));
-    
-    int isLeft = 1;
-    int isTrans = 0;
-    int method = 2;
-    fk::matrix<TestType> productLeftFull = fmwt*mat1;
-    auto const productLeft = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productLeftFull.print("full");	
-    productLeft.print("apply left");	
-    
-    SECTION("degree = 4, lev 5 fmwt apply left") { relaxed_comparison(productLeftFull, productLeft); }
-    
-  
-     
-    isLeft = 1;
-    isTrans = 1;
-    method=2;
-    fk::matrix<TestType> fmwt_transpose = fk::matrix<TestType>(fmwt).transpose();
-    fk::matrix<TestType> productLeftTransFull = fmwt_transpose*mat1;
-    auto const productLeftTrans = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    SECTION("degree = 2, lev 2 fmwt apply left transpose method 2") { relaxed_comparison(productLeftTrans, productLeftTransFull); }
-  
-    isLeft = 0;
-    isTrans = 0;
-    fk::matrix<TestType> productRightFull = mat1*fmwt;
-    auto const productRight = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    
-    SECTION("degree = 2, lev 2 fmwt apply right method 2") { relaxed_comparison(productRightFull, productRight); }
-    
-    isLeft = 0;
-    isTrans = 1;
-    fk::matrix<TestType> productRightTransFull = mat1*fmwt_transpose;
-    auto const productRightTrans = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    SECTION("degree = 2, lev 2 fmwt apply right transpose method 2") { relaxed_comparison(productRightTrans, productRightTransFull); }
-    
-   
-    method=3;
-    isLeft = 1;
-    isTrans = 0;
-    auto const productLeft3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productLeftFull.print("full");	
-    productLeft3.print("apply left 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply left method 3") { relaxed_comparison(productLeftFull, productLeft3); }
-    
-    isLeft = 1;
-    isTrans = 1;
-    auto const productLeftTrans3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productLeftTransFull.print("full");	
-    productLeftTrans3.print("apply left 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply left method 3") { relaxed_comparison(productLeftTransFull, productLeftTrans3); }
-    
-    isLeft = 0;
-    isTrans = 0;
-    auto const productRight3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productRightFull.print("full");	
-    productRight3.print("apply left 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply right method 3") { relaxed_comparison(productRightFull, productRight3); }
-    
-    isLeft = 0;
-    isTrans = 1;
-    auto const productRightTrans3 = apply_fmwt<TestType>(fmwt,mat1,kdeg,lev,isLeft,isTrans,method);
-    productRightTransFull.print("full");	
-    productRightTrans3.print("apply right trans 3");	
-    
-    SECTION("degree = 2, lev 2 fmwt apply right trans method 3") { relaxed_comparison(productRightTransFull, productRightTrans3); }
-  
-  }
 
+    int isLeft  = 1;
+    int isTrans = 0;
+    int method  = 2;
+    auto const productLeft1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productLeft2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply left - method 2")
+    {
+      relaxed_comparison(productLeft1, productLeft2);
+    }
+
+    isLeft  = 1;
+    isTrans = 1;
+    method  = 2;
+    auto const productLeftTrans1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productLeftTrans2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply left transpose - method 2")
+    {
+      relaxed_comparison(productLeftTrans1, productLeftTrans2);
+    }
+
+    isLeft  = 0;
+    isTrans = 0;
+    auto const productRight1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productRight2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply right - method 2")
+    {
+      relaxed_comparison(productRight1, productRight2);
+    }
+
+    isLeft  = 0;
+    isTrans = 1;
+    auto const productRightTrans1 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, 1);
+    auto const productRightTrans2 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply right transpose - method 2")
+    {
+      relaxed_comparison(productRightTrans1, productRightTrans2);
+    }
+
+    method  = 3;
+    isLeft  = 1;
+    isTrans = 0;
+    auto const productLeft3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply left - method 3")
+    {
+      relaxed_comparison(productLeft1, productLeft3);
+    }
+
+    isLeft  = 1;
+    isTrans = 1;
+    auto const productLeftTrans3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply left - method 3")
+    {
+      relaxed_comparison(productLeftTrans1, productLeftTrans3);
+    }
+
+    isLeft  = 0;
+    isTrans = 0;
+    auto const productRight3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply right - method 3")
+    {
+      relaxed_comparison(productRight1, productRight3);
+    }
+
+    isLeft  = 0;
+    isTrans = 1;
+    auto const productRightTrans3 =
+        apply_fmwt<TestType>(fmwt, mat1, kdeg, lev, isLeft, isTrans, method);
+    SECTION("degree = 4, lev 5 fmwt apply right trans - method 3")
+    {
+      relaxed_comparison(productRightTrans1, productRightTrans3);
+    }
+  }
 }
